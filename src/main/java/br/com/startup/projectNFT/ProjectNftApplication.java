30cb6f5246e0845d57ec2964c4f73b74dd4e853e
Lucas Rodrigues <lukinhasr1423@gmail.com>
2023-04-06 12:23:16 -0300

    first commit

---
+++ src/main/java/br/com/startup/projectNFT/ProjectNftApplication.java
@@ -0,0 +1,13 @@
+package br.com.startup.projectNFT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectNftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectNftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/startup/projectNFT/ProjectNftApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.startup.projectNFT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectNftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
